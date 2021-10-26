8841ae287eef711bbdd74db49762740bfa7c14c3
bobeer2406 <piotr.komor@gmail.com>
2021-10-26 13:32:31 +0200

    test

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Piotr's Grade Book");
+
+            while (true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+                if (input == "q")
+                {
+                    break;
+                }
+                try
+                {
+                     var grade = double.Parse(input);
+                     book.AddGrade(grade);                    
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    throw;
+                }
+                 finally
+                {
+                    Console.WriteLine("Thank Piotr");
+                }
+
+
+            }                           
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"The lowest grade is {stats.Low:N1}");
+            Console.WriteLine($"The highest grade is {stats.High:N1}");
+            Console.WriteLine($"The average grade us  {stats.Average:N1} ");    
+            Console.WriteLine($"The letter grade is {stats.Letter}");    
+        }
+    }
+}
