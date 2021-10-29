71f3e88f281a9b70fc51d4fdfc14ae4efd26d7c3
bobeer2406 <piotr.komor@gmail.com>
2021-10-29 15:51:22 +0200

    Defining a Event -start

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -80,7 +80,7 @@ namespace GradeBook
         public string Name
         {
           get;
-          private set;
+          set;
         }   
 
         public const string CATEGORY = "Science";        
